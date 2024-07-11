16483f3bd778e9bbf4d83f3be142361ebeaf2917
Lyutfi <lutfickl@hotmail.com>
2024-07-11 21:06:30 +0200

    jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/views/**/*.ejs'],
+  mode: 'jit',
+  purge: ['./src/views/**/*.ejs'],
   theme: {
     extend: {},
   },
